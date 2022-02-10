e321eb2b4c10e044ca94c6aff0dfc67fef5be39a
young <young@codberg.com>
2022-02-10 16:36:25 +0900

    material ui 설치 wrpa, header 틀 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Wrap from "./layouts/Wrap";
 
 function App() {
-  return <div>just do it</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Wrap />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Menu } from "@mui/icons-material";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__inner">
+        <div className="header__top">
+          <div className="header__top-nav">
+            <span>로그인</span>
+            <span>회원가입</span>
+          </div>
+        </div>
+      </div>
+      <div className="header__inner">
+        <div className="header__bottom">
+          <div></div>
+          <div className="header__bottom-logo">KICK BUCK</div>
+          <div className="header__bottom-menuBtn">
+            <Menu />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
