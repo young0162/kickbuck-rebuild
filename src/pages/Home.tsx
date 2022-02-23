084764dbfd9b8aab2439affb9dc843fbd2dbbaf3
young <young@codberg.com>
2022-02-23 18:17:59 +0900

    썸네일 사진 추가 및 img item map 전환중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../assets/scss/Home.scss";
 
 function Home() {
+  const [itemCount, setItemCount] = useState(20);
+  const [itemArray, setItemArray] = useState([0]);
+
+  useEffect(() => {
+    for (let i = 1; i < itemCount; i++) {
+      setItemArray((itemArray) => [...itemArray, i]);
+      console.log("i", i);
+    }
+  }, []);
+
   return (
     <div>
       <div className="img__wrap">
+        {itemArray.map((data, index) => {
+          <p key={index}>{data}</p>;
+        })}
         <div className="img__wrap-item">
-          <img src={require("../assets/img/craniosacral.jpg")} alt="img01" />
+          <img src={require("../assets/img/main-item1.jpg")} alt="img01" />
+        </div>
+        <div className="img__wrap-item">
+          <img src={require("../assets/img/main-item2.jpg")} alt="img01" />
+        </div>
+        <div className="img__wrap-item">
+          <img src={require("../assets/img/main-item3.jpg")} alt="img01" />
+        </div>
+        <div className="img__wrap-item">
+          <img src={require("../assets/img/main-item4.jpg")} alt="img01" />
+        </div>
+        <div className="img__wrap-item">
+          <img src={require("../assets/img/main-item5.jpg")} alt="img01" />
         </div>
       </div>
     </div>
